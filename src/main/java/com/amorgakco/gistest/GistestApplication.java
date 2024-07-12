ce05f8bb43e9e38aafc5042969b320b252e87809
songhaechan <bukak2019@naver.com>
2024-07-12 18:59:37 +0900

    first commit

---
+++ src/main/java/com/amorgakco/gistest/GistestApplication.java
@@ -0,0 +1,13 @@
+package com.amorgakco.gistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GistestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GistestApplication.class, args);
+	}
+
+}
